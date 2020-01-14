7d6b78a951bcc4f24b5773413542d1a3dc028c32
armin <arminkheirkhahan@gmail.com>
2020-01-14 10:58:10 +0330

    remove extra package

--- start/routes.js
+++ start/routes.js
@@ -16,7 +16,7 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use("Route");
 // const pdf2html = require("pdf2html");
-var pdftohtml = require("pdftohtmljs");
+// var pdftohtml = require("pdftohtmljs");
 
 // /** @type {import('@adonisjs/drive/src/DriveManager')} */
 // const Drive = use("Drive");
